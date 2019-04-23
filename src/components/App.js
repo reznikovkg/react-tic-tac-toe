51351b5534ec0ac1b5f540f7be5fb8f28dadb527
Reznikovkg <reznikov.kosta@yandex.ru>
2019-04-23 03:38:07 +0300

    work + redux-dev-tools

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
-import TitTacToe from "./TitTacToe";
+import { createStore } from 'redux';
+import  { Provider, connect } from 'react-redux';
+
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+const initialState = {
+    wallet: 10
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            let wallet = state.wallet + action.payload;
+            return { ...state, wallet };
+        case DECREMENT:
+            wallet = state.wallet - action.payload;
+            return { ...state, wallet };
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const mapDispatchToProps = dispatch => ({
+    onIncrement: (payload) => dispatch({ type: INCREMENT, payload }),
+    onDecrement: (payload) => dispatch({ type: DECREMENT, payload })
+});
+
+const mapStateToProps = state => ({
+    wallet: state.wallet
+});
+
+class Wallet extends Component {
+    render() {
+        return (
+            <div>
+                <div>Wallet: { this.props.wallet }</div>
+                <button onClick={ () => this.props.onIncrement(10) }>Increment</button>
+                <button onClick={ () => this.props.onDecrement(10) }>Decrement</button>
+            </div>
+        );
+    }
+}
+
+const WalletConnect = connect(mapStateToProps, mapDispatchToProps)(Wallet);
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <TitTacToe/>
-            </div>
+            <Provider store={ store }>
+                <WalletConnect/>
+            </Provider>
         );
     }
 }
